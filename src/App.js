6bd39ee0fec7ecac58d6c973c1cdb3b512002ab3
Geunmo Kim <kimgeunmo@gmail.com>
2022-03-19 23:21:10 +0100

    5.1 Tour of Create-React-App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+
 function App() {
   return (
     <div>
-      <h1>Initial Setup</h1>
+      <h1 className={styles.title}>Welcome to React</h1>
+      <Button text="Hello React" />
     </div>
   );
 }

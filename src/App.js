dc54a4ec7d44ba671f6ce28e6854e7b9677ba267
Geunmo Kim <kimgeunmo@gmail.com>
2022-03-20 20:08:29 +0100

    6.0 Introduction

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState(0);
+  const onClick = () => setValue((value) => value + 1);
+  console.log("render");
   return (
     <div>
-      <h1 className={styles.title}>Welcome to React</h1>
-      <Button text="Hello React" />
+      <h1>{value}</h1>
+      <button onClick={onClick}>Click Me</button>
     </div>
   );
 }
 
 export default App;
+
+/* 6.0 Introduction
+If you want to run a code only once on the initial render and keep it that way
+whether the state updates or not, usually when you are using an API.
+*/

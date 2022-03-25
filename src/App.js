226a611d263714f5f78029bca4e3d4fc63f96692
Geunmo Kim <kimgeunmo@gmail.com>
2022-03-25 14:57:08 +0100

    6.2 Deps

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [value, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((value) => value + 1);
-  console.log("I run all the time");
+  const onChange = (event) => setKeyword(event.target.value);
   useEffect(() => {
-    console.log("call the api");
-  }, []);
+    console.log("I run when 'keyword' changes.", keyword);
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'value' changes.", value);
+  }, [value]);
+  useEffect(() => {
+    console.log("I run when keyword & value change", keyword, value);
+  }, [keyword, value]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search Here..."
+      />
       <h1>{value}</h1>
       <button onClick={onClick}>Click Me</button>
     </div>
@@ -27,4 +40,11 @@ To run a code only once on the initial render, we can use useEffect();
 First we import {useEffect} from "react";
 useEffect() takes two arguments: first, the code that we want to run only once;
 The function in the first argument will occur only once at the first render;
+*/
+
+/* 6.2 Deps
+In the second argument of useEffect(), we put an array of consts;
+If the array is empty, the callbackFunction will only run once at the initial render;
+If there are any keyword in the array argument, the callbackFunction will run when there is
+any change in the const;
 */

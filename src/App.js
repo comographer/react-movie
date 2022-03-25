2e236c87e12f1d15fe0a2a1c43552b9a9d49c905
Geunmo Kim <kimgeunmo@gmail.com>
2022-03-25 14:07:12 +0100

    6.1 useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [value, setValue] = useState(0);
   const onClick = () => setValue((value) => value + 1);
-  console.log("render");
+  console.log("I run all the time");
+  useEffect(() => {
+    console.log("call the api");
+  }, []);
   return (
     <div>
       <h1>{value}</h1>
@@ -17,4 +20,11 @@ export default App;
 /* 6.0 Introduction
 If you want to run a code only once on the initial render and keep it that way
 whether the state updates or not, usually when you are using an API.
+*/
+
+/* 6.1 useEffect
+To run a code only once on the initial render, we can use useEffect();
+First we import {useEffect} from "react";
+useEffect() takes two arguments: first, the code that we want to run only once;
+The function in the first argument will occur only once at the first render;
 */
